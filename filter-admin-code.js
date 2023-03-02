ddf06db605c9aaa0304d6c55378ab49f8eb8068e
naoki <n.globe.us@gmail.com>
2023-03-02 16:28:35 +0900

    some fix

--- filter-admin-code.js
+++ filter-admin-code.js
@@ -2,26 +2,33 @@
 
 const fs = require('fs')
 const path = require('path')
-const { createArrayCsvWriter } = require('csv-writer')
+const { globSync } = require("glob")
+const { createArrayCsvWriter } = require('csv-writer');
 const { parse } = require('csv-parse/sync');
+
 
 async function exportCSV() {
 
   const file = fs.readFileSync(path.join(__dirname, 'output', 'admin-chiban-count.csv'), 'utf8')
   const raw = parse(file);
 
-  const adminCodeListRaw = fs.readFileSync(path.join(__dirname, 'output', 'admin-code-list.csv'), 'utf8')
-  const adminCodeList = parse(adminCodeListRaw);
+  const admins = globSync(`./admins/**/*.json`);
 
   const outCSV = []
 
-  for (let i = 0; i < adminCodeList.length; i++) {
-    const code = adminCodeList[i][0];
+  for (const admin of admins) {
+
+    const code = admin.split('/')[2].split('.')[0]
 
     const item = raw.find((item) => {
       return item[0] === code
     })
 
+    // 北方領土のコードは、法務省登記所備付地図データには存在しないのでスキップ
+    if (!item || item === undefined) {
+      console.log(`Not found: ${code}`)
+      continue
+    }
     outCSV.push(item)
   }
 
